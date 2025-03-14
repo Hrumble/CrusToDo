ea7caec68a216b7c38dd237e9d933f6e59017304
nolan <nolan.gero@gmail.com>
2025-03-14 22:57:08 +0100

    Added creating and checking for environment variable to store todo lists

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,15 @@ mod listmanager;
 
 use todolist::TodoList;
 use task::Task;
+use std::env::VarError;
+use std::error::Error;
+use std::fs;
 
+use std::path::PathBuf;
 use std::{env, process::exit};
 
 fn main() {
+    check_for_storage_path();
     parse_args();
 }
 
@@ -23,9 +28,32 @@ fn parse_args() -> Vec<String> {
 
 fn handle_args() {
     let args : Vec<String> = parse_args();
-    if &args[1] == "create" {
-
+    if &args[1] == "init" {
+       println!() 
     }
+}
+
+// This one checks if the environment variable CRUSTODO_PATH exists
+// if it doesn't then prompt user to create it, if another error, then exits program
+fn check_for_storage_path() -> PathBuf {
+     match env::var("CRUSTODO_PATH") {
+         Ok(val) => PathBuf::from(val),
+         Err(VarError::NotPresent) => {
+             println!("
+                 Your crustodo list path has not been set yet, this might be because
+                 this is the first time you've used crustodo, let's set it!
+
+                 Enter the path where you want to store your lists:
+             ");
+             let mut user_input : String = String::new();
+             std::io::stdin().read_line(&mut user_input).expect("Failed to read line");
+             return PathBuf::from(user_input);
+        }
+        Err(e) => {
+            eprintln!("There was an error reading the environment variable CRUSTODO_PATH : {e}");
+            exit(1);
+        }             
+     }
 }
 
 #[derive(PartialEq)]
